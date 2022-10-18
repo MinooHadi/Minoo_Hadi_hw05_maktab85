d02af99696a08234c5f5a399fbc9203d43f09a2b
Minoo <minoo.hadi2904@gmail.com>
2022-10-18 16:20:42 +0330

    create stepCounter object

---
+++ q2.js
@@ -0,0 +1,30 @@
+let stepCounter = {
+  step: 0,
+  increase: function () {
+    this.step++;
+    return this;
+  },
+  decrease: function () {
+    this.step--
+    return this;
+  },
+  reset: function () {
+    this.step = 0;
+    return this;
+  },
+  read: function () {
+    console.log(this.step);
+    return this;
+  },
+};
+stepCounter
+  .increase() //step = 1
+  .increase() //step = 2
+  .decrease() //step = 1
+  .increase() //step = 2
+  .decrease() //step = 1
+  .read() // 1
+  .reset() // step = 0
+  .read() // 0
+  .decrease() //step = -1
+  .read(); // -1
