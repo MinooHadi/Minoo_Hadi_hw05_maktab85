7d957a5a9fce2fa65230597f8eb2f2199bbbc3a5
Minoo <minoo.hadi2904@gmail.com>
2022-10-18 17:24:12 +0330

    rewrite ask function as arrow function

--- q5.js
+++ q5.js
@@ -20,3 +20,12 @@ ask(
   () => alert("You canceled the execution.")
 );
 
+
+
+// solution 2 - rewrite callback and ask functions as arrow functions
+
+let arrowAsk = (question, yes, no) => confirm(question) ? yes(): no()
+
+arrowAsk("Do you agree ?",
+() => alert("You agreed."),
+() => alert("You canceled the execution."))
